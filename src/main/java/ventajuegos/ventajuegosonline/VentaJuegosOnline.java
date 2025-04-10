dfc2028ef7465f6ac9b93cc191f45cd08dff344b
Boris Velizz <velizvelizb@gmail.com>
2025-04-10 15:59:28 -0400

    Venta de Juegos

---
+++ src/main/java/ventajuegos/ventajuegosonline/VentaJuegosOnline.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ventajuegos.ventajuegosonline;
+
+/**
+ *
+ * @author Boris Veliz
+ */
+public class VentaJuegosOnline {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
